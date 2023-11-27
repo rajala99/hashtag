54008bedb6aece115f060de2baa410241c9edbb1
rajala99 <rajala.harini99@gmail.com>
2023-11-27 20:09:22 +0530

    Added : Hashtag project - Harini

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -6,6 +6,7 @@ import Hashtag_Login from './Hashtag_Login';
 
 const togglePasswordText = () => {
     let txtPassValue = document.getElementById("txtPass");
+    console.log(txtPassValue)
     if (txtPassValue.type === "password") {
         txtPassValue.type = "text";
         document.getElementById("eyeCross").style.display = "block";
@@ -48,8 +49,8 @@ const Login = () => {
                                 <span class="input-group-text" id="addon-wrapping"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
                                     <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
                                 </svg></span>
-                                <input type="text" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="addon-wrapping" id='txtpass' onChange={(e) => setUserPass(e.target.value)} required />
-                                <span className='show-hide-password' onClick={() => togglePasswordText()}>
+                                <input type="password" class="form-control" placeholder="Password"  value={userPass} aria-label="Username" aria-describedby="addon-wrapping" id='txtpass' onChange={(e) => setUserPass(e.target.value)} required />
+                                <span className='show-hide-password' onClick={()=>togglePasswordText()}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                                         <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
                                         <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
